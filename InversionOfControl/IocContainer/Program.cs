d2487aebc0b5802c722e233fedeb0f807bc0e6ba
Raihan Nishat <39132907+raihannishat@users.noreply.github.com>
2024-07-11 01:30:38 +0600

    Update Program.cs

--- InversionOfControl/IocContainer/Program.cs
+++ InversionOfControl/IocContainer/Program.cs
@@ -1,9 +1,6 @@
-using IocContainer;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,9 +18,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
