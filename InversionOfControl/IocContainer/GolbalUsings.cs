d1409438a09682dc61e279c4c008c361525e6334
Raihan Nishat <39132907+raihannishat@users.noreply.github.com>
2024-07-11 01:32:58 +0600

    Update GolbalUsings.cs

--- InversionOfControl/IocContainer/GolbalUsings.cs
+++ InversionOfControl/IocContainer/GolbalUsings.cs
@@ -1,5 +1,5 @@
-﻿global using IocContainer.Services;
-global using Microsoft.AspNetCore.Mvc;
 global using IocContainer;
+global using IocContainer.Services;
+global using Microsoft.AspNetCore.Mvc;
 global using IocContainer.Attributes;
 global using System.Reflection;
